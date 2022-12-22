f29a368d1807e410f939ca4146eac1c7007ad288
coconut <1424392205@qq.com>
2022-12-22 09:28:49 +0800

    feat: 双指针技巧

--- 19.删除链表的倒数第-n-个结点.js
+++ 19.删除链表的倒数第-n-个结点.js
@@ -2,7 +2,7 @@
  * @Author: coconut 1424392205@qq.com
  * @Date: 2022-12-21 10:27:35
  * @LastEditors: coconut 1424392205@qq.com
- * @LastEditTime: 2022-12-21 11:35:20
+ * @LastEditTime: 2022-12-21 13:20:31
  * @FilePath: /leetcode-record/19.删除链表的倒数第-n-个结点.js
  * @Description:
  *
@@ -78,26 +78,55 @@
  * @param {number} k
  * @return {ListNode}
  */
+//要删除倒数第 n 个节点，就得获得倒数第 n + 1 个节点的引用，可以用我们实现的 findFromEnd 来操作。
+
+//不过注意我们又使用了虚拟头结点的技巧，也是为了防止出现空指针的情况，比如说链表总共有 5 个节点，题目就让你删除倒数第 5 个节点，也就是第一个节点，那按照算法逻辑，应该首先找到倒数第 6 个节点。但第一个节点前面已经没有节点了，这就会出错。
+
+//但有了我们虚拟节点 dummy 的存在，就避免了这个问题，能够对这种情况进行正确的删除。
 var removeNthFromEnd = function (head, n) {
-  let p1 = head;
-  let p2 = p1;
-  let p3 = p2;
-  if (p1.next == null) {
-    return null;
-  }
-  for (let i = 0; i < n; i++) {
-    p1 = p1.next;
-  }
-  if (p1 == null) {
-    p2 = p2.next;
-    return p2;
-  }
-  p1 = p1.next;
-  while (p1 !== null) {
-    p1 = p1.next;
-    p2 = p2.next;
-  }
-  p2.next = p2.next.next;
-  return p3;
+  // 虚拟头结点
+  let dummy = new ListNode();
+  dummy.next = head;
+  // 删除倒数第 n 个，要先找倒数第 n + 1 个节点
+  let x = findFromEnd(dummy, n + 1);
+  x.next = x.next.next;
+  return dummy.next;
 };
+
+let findFromEnd = function (head, k) {
+  let p1 = head;
+  // p1 先走 k 步
+  for (let i = 0; i < k; i++) {
+    p1 = p1.next;
+  }
+  let p2 = head;
+  while (p1 != null) {
+    p2 = p2.next;
+    p1 = p1.next;
+  }
+  // p2 现在指向第 n - k + 1 个节点，即倒数第 k 个节点
+  return p2;
+};
+// var removeNthFromEnd = function (head, n) {
+//   let p1 = head;
+//   let p2 = p1;
+//   let p3 = p2;
+//   if (p1.next == null) {
+//     return null;
+//   }
+//   for (let i = 0; i < n; i++) {
+//     p1 = p1.next;
+//   }
+//   if (p1 == null) {
+//     p2 = p2.next;
+//     return p2;
+//   }
+//   p1 = p1.next;
+//   while (p1 !== null) {
+//     p1 = p1.next;
+//     p2 = p2.next;
+//   }
+//   p2.next = p2.next.next;
+//   return p3;
+// };
 // @lc code=end
